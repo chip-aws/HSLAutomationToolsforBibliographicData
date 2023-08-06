808c0f31b50126886b65ecd89b8026dd0208ee57
UNCWellington <100953005+UNCWellington@users.noreply.github.com>
2023-08-06 14:44:21 -0400

    Delete boot.sh

--- boot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source venv/bin/activate
-#. venv/bin/activate
-export FLASK_APP=flasky.py
-export FLASK_DEBUG=1
-flask deploy
-# flask run
-# exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - flasky:app
-exec gunicorn -b 0.0.0.0:5000  - flasky:app
