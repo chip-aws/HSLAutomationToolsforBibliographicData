345dc3d7b8effffb6d3e74e263cf64a046457f35
UNCWellington <100953005+UNCWellington@users.noreply.github.com>
2023-08-06 19:21:20 -0400

    Update run_flask.sh

--- run_flask.sh
+++ run_flask.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 deactivate
 . venv/bin/activate
-# source venv/bin/activate
 export FLASK_APP=flasky.py
 export FLASK_DEBUG=1
-# flask run
-# flask run -h 127.0.0.1 -p 5000
-# gunicorn -b 0.0.0.0:5000  - flasky:app
 flask run --host=0.0.0.0
