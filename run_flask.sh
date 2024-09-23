bc91e9049b309b61de1ee6409ba03e38f3123385
Shouqiang Ye <100953005+UNCWellington@users.noreply.github.com>
2024-09-23 09:52:54 -0400

    Update run_flask.sh
    port 8000 -> 8080

--- run_flask.sh
+++ run_flask.sh
@@ -5,5 +5,5 @@ deactivate
 export FLASK_APP=flasky.py
 export FLASK_DEBUG=1
 # flask run
-flask run -h 0.0.0.0 -p 8000
-gunicorn -b 0.0.0.0:8000  - flasky:app
+flask run -h 0.0.0.0 -p 8080
+gunicorn -b 0.0.0.0:8080  - flasky:app
